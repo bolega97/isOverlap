d7fba7e46d9a49f2dc80dd99864a10c82ce02ca3
Bolega Ádám Balázs <h982094@stud.u-szeged.hu>
2023-02-21 17:26:51 +0100

    FInal

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,13 +33,9 @@ namespace AddDate
             this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.addBtn = new System.Windows.Forms.Button();
-            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
-            this.dateTimePicker4 = new System.Windows.Forms.DateTimePicker();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -56,7 +52,7 @@ namespace AddDate
             // 
             this.dateTimePicker2.CustomFormat = "dd-MM-yyyy";
             this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker2.Location = new System.Drawing.Point(340, 252);
+            this.dateTimePicker2.Location = new System.Drawing.Point(320, 252);
             this.dateTimePicker2.Name = "dateTimePicker2";
             this.dateTimePicker2.Size = new System.Drawing.Size(200, 22);
             this.dateTimePicker2.TabIndex = 1;
@@ -67,9 +63,7 @@ namespace AddDate
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
             this.Column2,
-            this.Column3,
-            this.Column4,
-            this.Column5});
+            this.Column3});
             this.dataGridView1.Location = new System.Drawing.Point(-5, 3);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
@@ -80,31 +74,13 @@ namespace AddDate
             // addBtn
             // 
             this.addBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.addBtn.Location = new System.Drawing.Point(574, 276);
+            this.addBtn.Location = new System.Drawing.Point(545, 249);
             this.addBtn.Name = "addBtn";
             this.addBtn.Size = new System.Drawing.Size(100, 32);
             this.addBtn.TabIndex = 3;
             this.addBtn.Text = "Hozzáad";
             this.addBtn.UseVisualStyleBackColor = true;
             this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
-            // 
-            // dateTimePicker3
-            // 
-            this.dateTimePicker3.CustomFormat = "dd-MM-yyyy";
-            this.dateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker3.Location = new System.Drawing.Point(94, 302);
-            this.dateTimePicker3.Name = "dateTimePicker3";
-            this.dateTimePicker3.Size = new System.Drawing.Size(200, 22);
-            this.dateTimePicker3.TabIndex = 4;
-            // 
-            // dateTimePicker4
-            // 
-            this.dateTimePicker4.CustomFormat = "dd-MM-yyyy";
-            this.dateTimePicker4.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker4.Location = new System.Drawing.Point(340, 302);
-            this.dateTimePicker4.Name = "dateTimePicker4";
-            this.dateTimePicker4.Size = new System.Drawing.Size(200, 22);
-            this.dateTimePicker4.TabIndex = 5;
             // 
             // Column1
             // 
@@ -116,43 +92,29 @@ namespace AddDate
             // Column2
             // 
             this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column2.HeaderText = "date1Start";
+            this.Column2.HeaderText = "date1";
             this.Column2.MinimumWidth = 6;
             this.Column2.Name = "Column2";
             // 
             // Column3
             // 
             this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column3.HeaderText = "date1End";
+            this.Column3.HeaderText = "date2";
             this.Column3.MinimumWidth = 6;
             this.Column3.Name = "Column3";
-            // 
-            // Column4
-            // 
-            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column4.HeaderText = "date2Start";
-            this.Column4.MinimumWidth = 6;
-            this.Column4.Name = "Column4";
-            // 
-            // Column5
-            // 
-            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column5.HeaderText = "date2End";
-            this.Column5.MinimumWidth = 6;
-            this.Column5.Name = "Column5";
             // 
             // form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dateTimePicker4);
-            this.Controls.Add(this.dateTimePicker3);
             this.Controls.Add(this.addBtn);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.dateTimePicker2);
             this.Controls.Add(this.dateTimePicker1);
             this.Name = "form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "isOverlap";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
@@ -165,13 +127,9 @@ namespace AddDate
         private System.Windows.Forms.DateTimePicker dateTimePicker2;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button addBtn;
-        private System.Windows.Forms.DateTimePicker dateTimePicker3;
-        private System.Windows.Forms.DateTimePicker dateTimePicker4;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,7 +12,7 @@ namespace AddDate
 {
     public partial class form1 : Form
     {
-      
+         List<(DateTime, DateTime)> list = new List<(DateTime, DateTime)>();
         public form1()
         {
             InitializeComponent();
@@ -21,28 +21,41 @@ namespace AddDate
         {
             return start1 < end2 && end1 > start2;
         }
+
+        public bool HasOverlapListCheck(DateTime date1, DateTime date2)
+        {
+             bool overlap=false;
+            for(int i = 0; i < list.Count; i++){
+                if (HasOverlap(list[i].Item1, list[i].Item2, date1, date2) ) {
+                    overlap = true;
+                }
+            }
+            return overlap;
+        }
+
+        private void addDataToDataGridView() {
+            int n = dataGridView1.Rows.Add();
+            dataGridView1.Rows[n].Cells[0].Value = (n + 1).ToString();
+            dataGridView1.Rows[n].Cells[1].Value = dateTimePicker1.Value.ToString("dd-MM-yyyy");
+            dataGridView1.Rows[n].Cells[2].Value = dateTimePicker2.Value.ToString("dd-MM-yyyy");
+        }
         private void addBtn_Click(object sender, EventArgs e)
         {
-            
-            
-         
-            if (!HasOverlap(dateTimePicker1.Value, dateTimePicker2.Value, dateTimePicker3.Value, dateTimePicker4.Value))
-            {
-                int n = dataGridView1.Rows.Add();
-                dataGridView1.Rows[n].Cells[0].Value = (n + 1).ToString();
-                dataGridView1.Rows[n].Cells[1].Value = dateTimePicker1.Value.ToString("dd-MM-yyyy");
-                dataGridView1.Rows[n].Cells[2].Value = dateTimePicker2.Value.ToString("dd-MM-yyyy");
-                dataGridView1.Rows[n].Cells[3].Value = dateTimePicker3.Value.ToString("dd-MM-yyyy");
-                dataGridView1.Rows[n].Cells[4].Value = dateTimePicker4.Value.ToString("dd-MM-yyyy");
+            if (list.Count == 0) {
 
+                list.Add((dateTimePicker1.Value, dateTimePicker1.Value));
+                addDataToDataGridView();
+               
+            }
+            else if (!HasOverlapListCheck(dateTimePicker1.Value, dateTimePicker2.Value)) {
+
+                list.Add((dateTimePicker1.Value, dateTimePicker1.Value));
+                addDataToDataGridView();
             }
             else {
                 MessageBox.Show("Átfedés!", "Hiba!", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
-
-            
-
         }
     }
 }
